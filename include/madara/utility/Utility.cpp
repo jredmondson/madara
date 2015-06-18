f6b5ca22515e6a7358af25692f34d05c99783cc7
James Edmondson <jedmondson@gmail.com>
2015-06-18 15:21:34 -0400

    * JAVA BUGFIX: using HOST_NAME_MAX

--- include/madara/utility/Utility.cpp
+++ include/madara/utility/Utility.cpp
@@ -389,8 +389,8 @@ Madara::Utility::bind_to_ephemeral_port (ACE_SOCK_Dgram & socket,
   // increase port each time we don't properly bind
 
   ACE_INET_Addr addr (port);
-  char hostname[128] = "";
-  addr.get_host_name (hostname, 128);
+  char hostname[HOST_NAME_MAX] = "";
+  addr.get_host_name (hostname, HOST_NAME_MAX);
   host = hostname;
 
   ACE_OS::socket_init (2, 2);
