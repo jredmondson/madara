c41092c3094a7d28914c4de2708625693dfd0a95
James Edmondson <jedmondson@gmail.com>
2015-06-22 16:56:51 -0400

    * ANDROID BUGS: Trying to get logger working on Android

--- include/madara/logger/Logger.cpp
+++ include/madara/logger/Logger.cpp
@@ -1,4 +1,6 @@
 #include "Logger.h"
+#include <stdio.h>
+#include <stdarg.h>
 
 Madara::Logger::Logger::Logger (bool log_to_terminal)
 : mutex_ (), level_ (LOG_EMERGENCY),
@@ -23,52 +25,41 @@ Madara::Logger::Logger::log (int level, const char * message, ...)
     va_list argptr;
     va_start (argptr, message);
 
+    // Android seems to not handle printf arguments correctly as best I can tell
+    char buffer[10240];
+    vsnprintf (buffer, sizeof(buffer), message, argptr);
+
+    va_end (argptr);
+
     Guard guard (mutex_);
 
 #ifdef _MADARA_ANDROID_
     if (this->term_added_ || this->syslog_added_)
     {
-      va_list localargs;
-      va_copy (localargs, argptr);
-
       if (level == LOG_ERROR)
       {
-        __android_log_print (ANDROID_LOG_ERROR, "madara", message, localargs);
+        __android_log_write (ANDROID_LOG_ERROR, tag_.c_str (), buffer);
       }
       else if (level == LOG_WARNING)
       {
-        __android_log_print (ANDROID_LOG_WARN, "madara", message, localargs);
+        __android_log_write (ANDROID_LOG_WARN, tag_.c_str (), buffer);
       }
       else
       {
-        __android_log_print (ANDROID_LOG_INFO, "madara", message, localargs);
+        __android_log_write (ANDROID_LOG_INFO, tag_.c_str (), buffer);
       }
-
-      va_end (localargs);
     }
 #else // end if _USING_ANDROID_
     if (this->term_added_ || this->syslog_added_)
     {
-      va_list localargs;
-      va_copy (localargs, argptr);
-
-      vfprintf (stderr, message, localargs);
-
-      va_end (localargs);
+      fprintf (stderr, buffer);
     }
 #endif
 
     for (File_Vectors::iterator i = files_.begin ();
       i != files_.end (); ++i)
     {
-      va_list localargs;
-      va_copy (localargs, argptr);
-
-      vfprintf (*i, message, localargs);
-
-      va_end (localargs);
+      fprintf (*i, message);
     }
-
-    va_end (argptr);
   }
 }
