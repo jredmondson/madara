f64e9ea7e7e081c6dda89650bd773d608229f0cf
James Edmondson <jedmondson@gmail.com>
2015-10-11 17:03:21 -0400

    * BUGFIX: Updated madara_jni.h to define proper symbol exports for JNI_OnLoad and JNI_OnUnload

--- port/java/jni/madara_jni.h
+++ port/java/jni/madara_jni.h
@@ -5,8 +5,8 @@
 #include <jni.h>
 #include "madara/MADARA_export.h"
 
-MADARA_Export jint JNICALL JNIOnLoad (JavaVM *vm, void *reserved);
-MADARA_Export void JNICALL JNIOnUnload(JavaVM* vm, void* reserved);
+MADARA_Export jint JNICALL JNI_OnLoad (JavaVM *vm, void *reserved);
+MADARA_Export void JNICALL JNI_OnUnload(JavaVM* vm, void* reserved);
 
 MADARA_Export bool madara_jni_is_attached ();
 MADARA_Export JNIEnv* madara_jni_get_env();
