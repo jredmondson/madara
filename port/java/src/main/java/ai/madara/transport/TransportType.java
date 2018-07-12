4ed5cce783c085b4aa881569fff41cf75d9c4349
James Edmondson <jedmondson@gmail.com>
2018-07-12 13:53:24 -0400

    * Making enum for ZMQ more consistent

--- port/java/src/main/java/ai/madara/transport/TransportType.java
+++ port/java/src/main/java/ai/madara/transport/TransportType.java
@@ -57,7 +57,7 @@ public enum TransportType
   BROADCAST_TRANSPORT(6),
   REGISTRY_SERVER(7),
   REGISTRY_CLIENT(8),
-  ZMQ(9),
+  ZMQ_TRANSPORT(9),
   INCONSISTENT_TRANSPORT(100);
 
   private int num;
