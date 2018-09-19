dfabd9ddbbf8a86bfbc42d620b8b639caaeef144
James Edmondson <jedmondson@gmail.com>
2018-09-19 01:14:03 -0400

    * Fixing style of Logger

--- include/madara/logger/Logger.cpp
+++ include/madara/logger/Logger.cpp
@@ -7,12 +7,12 @@
 #include <iomanip>
 
 #ifndef MADARA_NO_THREAD_LOCAL
-thread_local int madara::logger::Logger::thread_level_(
+thread_local int madara::logger::Logger::thread_level_ (
   madara::logger::TLS_THREAD_LEVEL_DEFAULT);
 
-thread_local std::string madara::logger::Logger::thread_name_("");
+thread_local std::string madara::logger::Logger::thread_name_ ("");
 
-thread_local double madara::logger::Logger::thread_hertz_(
+thread_local double madara::logger::Logger::thread_hertz_ (
   madara::logger::TLS_THREAD_HZ_DEFAULT);
 #endif
 
@@ -37,17 +37,17 @@ madara::logger::Logger::~Logger ()
 
 
 std::string
-madara::logger::Logger::strip_custom_tstamp(
+madara::logger::Logger::strip_custom_tstamp (
   const std::string in_str,
   const std::string ts_str)
 {
-  std::string ret_str(in_str);
-  ret_str.replace(ret_str.find(ts_str), ts_str.length(), ts_str);
+  std::string ret_str (in_str);
+  ret_str.replace (ret_str.find (ts_str), ts_str.length (), ts_str);
   return ret_str;
 }
 
 std::string 
-madara::logger::Logger::search_and_insert_custom_tstamp(
+madara::logger::Logger::search_and_insert_custom_tstamp (
   const std::string & buf,
   const std::string & ts_str)
 {
@@ -70,7 +70,7 @@ madara::logger::Logger::search_and_insert_custom_tstamp(
      * buffer
      * 
      **/
-    found = ret_string.find(ts_str.c_str(), found + offset, ts_str.length());
+    found = ret_string.find (ts_str.c_str (), found + offset, ts_str.length ());
     if ( found == std::string::npos )
     {
       done = true;
@@ -89,20 +89,20 @@ madara::logger::Logger::search_and_insert_custom_tstamp(
       
       /// convert the double into string format that can easily be written
       /// into the copy of the message buffer.
-      mgt_str << std::setprecision(MGT_DIGIT_PRECISION) <<
+      mgt_str << std::setprecision (MGT_DIGIT_PRECISION) <<
         std::fixed << mgt_time;
-      ret_string.replace(ret_string.find(ts_str), ts_str.length(),
-        mgt_str.str());
+      ret_string.replace (ret_string.find (ts_str), ts_str.length (),
+        mgt_str.str ());
       continue;
     }
     else if ( ts_str == MADARA_THREAD_NAME_ )
     {
       //insert thread name into message buffer copy
 #ifndef MADARA_NO_THREAD_LOCAL
-      ret_string.replace(ret_string.find(ts_str), ts_str.length(),
-                        madara::logger::Logger::get_thread_name());
+      ret_string.replace (ret_string.find (ts_str), ts_str.length (),
+                        madara::logger::Logger::get_thread_name ());
 #else
-      ret_string.replace(ret_string.find(ts_str), ts_str.length(),
+      ret_string.replace (ret_string.find (ts_str), ts_str.length (),
                         "<DISABLED>");
 #endif
       continue;
@@ -112,11 +112,11 @@ madara::logger::Logger::search_and_insert_custom_tstamp(
       // insert thread hertz value into message buffer copy
 #ifndef MADARA_NO_THREAD_LOCAL
       std::string hzstr =
-        boost::lexical_cast<std::string>(
-          madara::logger::Logger::get_thread_hertz());
-      ret_string.replace(ret_string.find(ts_str), ts_str.length(), hzstr);
+        boost::lexical_cast<std::string> (
+          madara::logger::Logger::get_thread_hertz ());
+      ret_string.replace (ret_string.find (ts_str), ts_str.length (), hzstr);
 #else
-      ret_string.replace(ret_string.find(ts_str), ts_str.length(),
+      ret_string.replace (ret_string.find (ts_str), ts_str.length (),
                         "<DISABLED>");
 #endif
       continue;
@@ -153,12 +153,12 @@ madara::logger::Logger::log (
        * key string data embedded the number of times it was used.
       **/
       std::string mad_str = message;
-      mad_str = search_and_insert_custom_tstamp(mad_str, MADARA_GET_TIME_MGT_);
-      mad_str = search_and_insert_custom_tstamp(mad_str, MADARA_THREAD_NAME_);
-      mad_str = search_and_insert_custom_tstamp(mad_str, MADARA_THREAD_HERTZ_);
+      mad_str = search_and_insert_custom_tstamp (mad_str, MADARA_GET_TIME_MGT_);
+      mad_str = search_and_insert_custom_tstamp (mad_str, MADARA_THREAD_NAME_);
+      mad_str = search_and_insert_custom_tstamp (mad_str, MADARA_THREAD_HERTZ_);
     
       char custom_buffer[10240];
-      std::strcpy(custom_buffer, mad_str.c_str());
+      std::strcpy (custom_buffer, mad_str.c_str ());
 
       /**
        * Prepare string to write into copy of the message buffer.
@@ -173,10 +173,10 @@ madara::logger::Logger::log (
       time (&raw_time);
       time_info = localtime (&raw_time);
 
-      mad_str = search_and_insert_custom_tstamp(timestamp_format_,
+      mad_str = search_and_insert_custom_tstamp (timestamp_format_,
         MADARA_GET_TIME_MGT_);
-      mad_str = search_and_insert_custom_tstamp(mad_str, MADARA_THREAD_NAME_);
-      mad_str = search_and_insert_custom_tstamp(mad_str, MADARA_THREAD_HERTZ_);
+      mad_str = search_and_insert_custom_tstamp (mad_str, MADARA_THREAD_NAME_);
+      mad_str = search_and_insert_custom_tstamp (mad_str, MADARA_THREAD_HERTZ_);
       
       /**
        * Process the normal message buffer and write into final copy to
@@ -188,9 +188,12 @@ madara::logger::Logger::log (
       remaining_buffer -= chars_written;
       begin += chars_written;
       vsnprintf (begin, remaining_buffer, custom_buffer, argptr);
-    }else
+    }
+    else
+    {
       vsnprintf (begin, remaining_buffer, message, argptr);
-
+    }
+    
     va_end (argptr);
 
     MADARA_GUARD_TYPE guard (mutex_);
