63be30a63efe1201122782bfdd2e17bd0ec7c389
James Edmondson <jedmondson@gmail.com>
2018-10-07 17:50:03 -0400

    * Modified get_modifieds_current default arguments to not include the send_list

--- include/madara/knowledge/ThreadSafeContext.h
+++ include/madara/knowledge/ThreadSafeContext.h
@@ -890,7 +890,7 @@ public:
    * @return  the modified knowledge records
    **/
   KnowledgeMap get_modifieds_current(
-      const std::map<std::string, bool> & send_list = {}, bool reset = true);
+      const std::map<std::string, bool> & send_list, bool reset = true);
 
   /**
    * Adds a list of VariableReferences to the current modified list.
