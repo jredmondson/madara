13a5875725cda3ba2680c19c238289e8d280b583
Vibhav Ganesh <vibhav.ganesh@gmail.com>
2018-10-10 17:54:54 -0700

    Adding special lock that logs what is locking it

--- include/madara/LockType.h
+++ include/madara/LockType.h
@@ -24,11 +24,40 @@ struct null_mutex
 #define MADARA_LOCK_TYPE madara::null_mutex
 #define MADARA_LOCK_LOCK lock
 #define MADARA_LOCK_UNLOCK unlock
+
+#elif defined _MADARA_LOG_LOCK_
+
+#include <iostream>
+#include <pthread.h>
+
+struct lock_logger : public std::recursive_mutex
+{
+  lock_logger() : name_("Unamed") {}
+  lock_logger(std::string name) : name_(name) {}
+
+  void lock()
+  {
+    char thread_name[16];
+    pthread_getname_np(pthread_self(), thread_name, 16);
+    std::cerr << "(TRYING) Thread " << std::string(thread_name) << " is locking mutex " << name_ << std::endl;
+    std::recursive_mutex::lock();
+    std::cerr << "(GOT IT) Thread " << std::string(thread_name) << " is locking mutex " << name_ << std::endl;
+  }
+
+  private:
+    std::string name_;
+};
+
+#define MADARA_LOCK_TYPE lock_logger
+#define MADARA_LOCK_LOCK lock
+#define MADARA_LOCK_UNLOCK unlock
 #else
+
 #define MADARA_LOCK_TYPE std::recursive_mutex
 #define MADARA_LOCK_LOCK lock
 #define MADARA_LOCK_UNLOCK unlock
 #endif  // !_MADARA_NULL_LOCK_
+
 
 #define MADARA_GUARD_TYPE std::lock_guard<MADARA_LOCK_TYPE>
 #define MADARA_CONDITION_TYPE std::condition_variable_any
