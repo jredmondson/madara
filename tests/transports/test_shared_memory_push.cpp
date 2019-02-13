5ff78aedbcc9c1dc06639940fa1fa85a150b72ed
James Edmondson <jedmondson@gmail.com>
2019-02-13 00:35:45 -0500

    * Added more tests to the SharedMemoryPush test * Added the SharedMemoryPush test to Travis CI

--- tests/transports/test_shared_memory_push.cpp
+++ tests/transports/test_shared_memory_push.cpp
@@ -28,15 +28,55 @@ int main (int, char **)
 
     transports[i]->set(kbs);
 
-    kbs[i].attach_transport (transports[i]);
+    kbs[i].attach_transport(transports[i]);
   }
 
   kbs[0].set("agent.0.name", "Sam Brandy", knowledge::EvalSettings::SEND);
 
-  std::cerr << "Checking first push results: agent.0.name == Sam Brandy: ";
+  std::cerr << "Test 1: from agent.0: agent.0.name == Sam Brandy: ";
 
   if (kbs[1].get("agent.0.name") == "Sam Brandy" && 
       kbs[2].get("agent.0.name") == "Sam Brandy")
+  {
+    std::cerr << "SUCCESS\n";
+  }
+  else
+  {
+    std::cerr << "FAIL\n";
+    madara_fails++;
+
+    for (auto kb : kbs)
+    {
+      kb.print();
+    }
+  }
+
+  kbs[1].set("agent.1.name", "Bob Franklin", knowledge::EvalSettings::SEND);
+
+  std::cerr << "Test 2: from agent.1: agent.1.name == Bob Franklin: ";
+
+  if (kbs[0].get("agent.1.name") == "Bob Franklin" && 
+      kbs[2].get("agent.1.name") == "Bob Franklin")
+  {
+    std::cerr << "SUCCESS\n";
+  }
+  else
+  {
+    std::cerr << "FAIL\n";
+    madara_fails++;
+
+    for (auto kb : kbs)
+    {
+      kb.print();
+    }
+  }
+
+  kbs[2].set("agent.2.name", "Sara Thompkins", knowledge::EvalSettings::SEND);
+
+  std::cerr << "Test 3: from agent.2: agent.2.name == Sara Thompkins: ";
+
+  if (kbs[0].get("agent.2.name") == "Sara Thompkins" && 
+      kbs[1].get("agent.2.name") == "Sara Thompkins")
   {
     std::cerr << "SUCCESS\n";
   }
