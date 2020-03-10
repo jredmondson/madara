f95abf80e74c53f252c154aa4d19704ffc75e942
James Edmondson <jedmondson@gmail.com>
2020-03-09 18:06:06 -0700

    * Added support for MADARA_MAKE_VAL_SUPPORT_TEST

--- include/madara/utility/StlHelper.h
+++ include/madara/utility/StlHelper.h
@@ -9,6 +9,52 @@
 
 #include <type_traits>
 
+/**
+ * Macro which generates feature testing traits, to allow enabling features
+ * based on what a given type supports. The tests provide ::value member
+ * which is true if the given expr can compile correctly with the given
+ * type; false otherwise
+ *
+ * var is a pointer to the type being tested
+ */
+#define MADARA_MAKE_SUPPORT_TEST(name, var, expr)                   \
+  template<typename T>                                              \
+  struct supports_##name##_impl                                     \
+  {                                                                 \
+    template<typename U>                                            \
+    static auto test(U* var) -> decltype((expr), std::true_type()); \
+    template<typename U>                                            \
+    static auto test(...) -> std::false_type;                       \
+    using type = decltype(test<T>(0));                              \
+  };                                                                \
+  template<typename T>                                              \
+  struct supports_##name : supports_##name##_impl<T>::type          \
+  {                                                                 \
+  }
+
+/**
+ * Macro which generates feature testing traits, to allow enabling features
+ * based on what a given type supports. The tests provide ::value member
+ * which is true if the given expr can compile correctly with the given
+ * type; false otherwise
+ *
+ * var is a value of the type being tested
+ */
+#define MADARA_MAKE_VAL_SUPPORT_TEST(name, var, expr)              \
+  template<typename T>                                             \
+  struct supports_##name##_impl                                    \
+  {                                                                \
+    template<typename U>                                           \
+    static auto test(U var) -> decltype((expr), std::true_type()); \
+    template<typename U>                                           \
+    static auto test(...) -> std::false_type;                      \
+    using type = decltype(test<T>(std::declval<T>()));             \
+  };                                                               \
+  template<typename T>                                             \
+  struct supports_##name : supports_##name##_impl<T>::type         \
+  {                                                                \
+  }
+
 namespace madara
 {
 namespace utility
