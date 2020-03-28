345dcd3050379b3fd3589e4f5d80a35963923422
James Edmondson <jedmondson@gmail.com>
2020-03-27 23:24:08 -0700

    * Fixed issues with SIMTIME linking

---
+++ cmake/configuration.cmake
@@ -0,0 +1,17 @@
+
+# Options
+option(madara_ANDROID "Build with Android support" OFF)
+option(madara_DOCS "Run doxygen or javadocs on code" OFF)
+option(madara_KARL "Build with KaRL scripting language support (eval/wait)" ON)
+option(madara_LZ4 "Build with LZ4 support" OFF)
+option(madara_NDDS "Build with RTI NDDS support" OFF)
+option(madara_PYTHON "Build with python support (via Boost.Python)" OFF)
+option(madara_SIMTIME "Build with support for simulation/virtual time" ON)
+option(madara_SPLICE "Build with PrismTech OpenSplice DDS support" OFF)
+option(madara_SSL "Build with SSL support" OFF)
+option(madara_TESTS "Build tests" OFF)
+option(madara_THREADLOCAL "Build with support for threadlocal variables" OFF)
+option(madara_TUTORIALS "Build tutorials" OFF)
+option(madara_XML "Build with XML library support" OFF)
+option(madara_ZMQ "Build with ZMQ/0MQ support" OFF)
+
