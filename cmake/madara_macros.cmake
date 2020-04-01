9c6a98fe8ff3b1be4ef3f4669b3bf89329aad67d
James Edmondson <jedmondson@gmail.com>
2020-03-31 17:37:44 -0700

    * Added size and other features to Hive * Added install options for tests and tools by default

--- cmake/madara_macros.cmake
+++ cmake/madara_macros.cmake
@@ -2,9 +2,14 @@
 macro(madara_app _NAME _FILE)
   add_executable(${_NAME} ${_FILE})
   target_link_libraries(${_NAME} PRIVATE madara)
+  install(TARGETS ${_NAME} EXPORT madaraTargets
+           RUNTIME DESTINATION ${GAMS_RUNTIME_INSTALL_DIR})
+
 endmacro()
 
 macro(madara_test _NAME _FILE)
   add_executable(${_NAME} ${_FILE})
   target_link_libraries(${_NAME} PRIVATE madara)
+  install(TARGETS ${_NAME} EXPORT madaraTargets
+           RUNTIME DESTINATION ${GAMS_RUNTIME_INSTALL_DIR})
 endmacro()
