2e8435fea9c3bffa2df7ed5eaae747a3af52c230
James Edmondson <jedmondson@gmail.com>
2023-12-22 13:17:40 -0800

    * Attempting to fix sphinx and doxygen integration

--- docs/conf.py
+++ docs/conf.py
@@ -16,6 +16,7 @@
 # import sys
 # sys.path.insert(0, os.path.abspath('.'))
 import subprocess
+subprocess.call('cd .. ; doxygen', shell=True)
 
 # -- Project information -----------------------------------------------------
 
